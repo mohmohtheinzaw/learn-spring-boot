aa46223f363344ad43d2cff6b442c2a4381ec8e7
Moh Moh Thein Zaw <mohmohtheinzaw379@gmail.com>
2024-04-23 11:13:26 +0630

    feat(app): add admin model

---
+++ src/main/java/com/test/test/model/Admin.java
@@ -0,0 +1,34 @@
+package com.test.test.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username",nullable = false)
+    private String username;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Column(name = "createdAt")
+    @CreationTimestamp
+    private Date createdAt;
+
+    @Column(name = "updatedAt")
+    @UpdateTimestamp
+    private Date updatedAt;
+}
+
